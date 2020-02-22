f0e7d4c719906f4bdc12e393e17fa0524ccc944c
Sara <sbanaeirad@my.bcit.ca>
2020-02-22 15:39:13 -0800

    test fetch data

--- Bookafe/Bookafe/ClientApp/src/components/NavMenu.js
+++ Bookafe/Bookafe/ClientApp/src/components/NavMenu.js
@@ -38,7 +38,7 @@ export class NavMenu extends Component {
                   <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/search">Search</NavLink>
                 </NavItem>
                 <LoginMenu>
                 </LoginMenu>
---
+++ Bookafe/Bookafe/ClientApp/src/components/Search.js
@@ -0,0 +1,47 @@
+﻿import React, { Component } from 'react';
+
+
+export class Search extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchByTitle:[],
+        };
+        this.getAll = this.getAll.bind(this);
+    }
+
+    componentDidMount() {
+        this.getAll();
+    }
+
+
+
+    getAll() {
+        const URL = "https://www.googleapis.com/books/v1/volumes?q=untitle:harrypotter&maxResults=3"
+        // This code gets data from the remote server.
+        fetch(URL)
+            .then(response => response.json())
+
+            // Data Retrieved.
+            .then(data => {
+                console.log(JSON.stringify(data));
+                this.setState({ searchByTitle: data.items });
+            })
+
+            // Data Not Retrieved.
+            .catch(error => {
+                alert(error);
+            });
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.state.searchByTitle.map((item, index) => (
+                    <li key={item.id}>{item.volumeInfo.imageLinks.thumbnail}</li>
+
+                ))}
+            </ul>
+            );
+    }
+}
