5bbf3404272397418707bb127cbe098d3d357416
Sara <sbanaeirad@my.bcit.ca>
2020-02-27 00:06:01 -0800

    Edit search

--- Bookafe/Bookafe/ClientApp/src/components/Search.js
+++ Bookafe/Bookafe/ClientApp/src/components/Search.js
@@ -4,48 +4,45 @@ export class Search extends Component {
     constructor() {
         super();
         this.state = {
-            searchByTitle: [],
-            searchByAuthor: []
+            searchResult: [],
         };
-        this.getAllTitle = this.getAllTitle.bind(this);
-        this.getAllAuthor = this.getAllAuthor.bind(this);
+        this.getAll = this.getAll.bind(this);
     }
 
     //componentDidMount() {
     //    this.getAll();
     //}
 
-    getAllTitle() {
+    getAll() {
         let title = this.titleToSearch.value;
-        const URL = "https://www.googleapis.com/books/v1/volumes?q=untitle:"+title+"&maxResults=10"
-        // This code gets data from the remote server.
-        fetch(URL)
-            .then(response => response.json())
-            // Data Retrieved.
-            .then(data => {
-                //console.log(JSON.stringify(data));
-                console.log(URL)
-                this.setState({ searchByTitle: data.items });
-                console.log(URL)
-            })
+        let author = this.auhtorToSearch.value;
 
-            // Data Not Retrieved.
-            .catch(error => {
-                alert(error);
-            });
+         if (title && author) {
+
+            var URL = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + title +"+inauthor:" + author + "&maxResults=15"
+         }
+
+          else if (title) {
+
+             var URL = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + title + "&maxResults=15"
     }
 
-    getAllAuthor() {
-        let author = this.auhtorToSearch.value;
-        const URL = "https://www.googleapis.com/books/v1/volumes?q=unauthor:" + author + "&maxResults=10"
+          else if (author) {
+
+            var URL = "https://www.googleapis.com/books/v1/volumes?q=inauthor:" + author + "&maxResults=15"
+         }
+        else {
+            alert("please ");
+        }
+        
         // This code gets data from the remote server.
         fetch(URL)
             .then(response => response.json())
             // Data Retrieved.
             .then(data => {
-                //console.log(JSON.stringify(data));
+                console.log(JSON.stringify(data));
                 console.log(URL)
-                this.setState({ searchByAuthor: data.items });
+                this.setState({ searchResult: data.items });
                 console.log(URL)
             })
 
@@ -63,39 +60,27 @@ export class Search extends Component {
                     placeholder= "title"
                     ref={getTitleInput => (this.titleToSearch = getTitleInput)}
                 />
-                <button onClick={this.getAllTitle}>Search By Title</button>
+                <br />
+                <input
+                    type="text"
+                    placeholder="author"
+                    ref={getAuthorInput => (this.auhtorToSearch = getAuthorInput)}
+                />
+                <br />
+                <button onClick={this.getAll}>Search by Author</button>
                 <br />
               <table class="table table-sm table-dark">
                 <tr><th>Title</th><th>Author</th><th>Rating</th><th>Image</th></tr>
                 {
-                    this.state.searchByTitle.map((data) =>
+                    this.state.searchResult.map((data) =>
                         <tr>
                             <td>  {data.volumeInfo.title} </td>
                             <td>  {data.volumeInfo.authors[0]} </td>
                             <td>  {data.volumeInfo.averageRating} </td>
                             <td>  <img src={data.volumeInfo.imageLinks.thumbnail} alt="coverimage" height="100" width="140"/> </td>
                         </tr>
-                    )}
-              </table>
-                <input
-                    type="text"
-                    placeholder="author"
-                    ref={getAuthorInput => (this.auhtorToSearch = getAuthorInput)}
-                />
-                <button onClick={this.getAllAuthor}>Search by Author</button>
-                <br />
-                <table class="table table-sm table-dark">
-                    <tr><th>Title</th><th>Author</th><th>Rating</th><th>Image</th></tr>
-                    {
-                        this.state.searchByAuthor.map((data) =>
-                            <tr>
-                                <td>  {data.volumeInfo.title} </td>
-                                <td>  {data.volumeInfo.authors[0]} </td>
-                                <td>  {data.volumeInfo.averageRating} </td>
-                                <td>  <img src={data.volumeInfo.imageLinks.thumbnail} alt="coverimage" height="100" width="140" /> </td>
-                            </tr>
                         )}
-                </table>
+                    </table>
             </div>
             );
     }
