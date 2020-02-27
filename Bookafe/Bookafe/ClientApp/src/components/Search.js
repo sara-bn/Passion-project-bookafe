f4abfd6d1f8d1764441e83aa303c6f3c68e7b4f9
Sara <sbanaeirad@my.bcit.ca>
2020-02-26 23:02:09 -0800

    seperated search for author and title

--- Bookafe/Bookafe/ClientApp/src/components/Search.js
+++ Bookafe/Bookafe/ClientApp/src/components/Search.js
@@ -4,26 +4,49 @@ export class Search extends Component {
     constructor() {
         super();
         this.state = {
-            searchByTitle:[],
+            searchByTitle: [],
+            searchByAuthor: []
         };
-        this.getAll = this.getAll.bind(this);
+        this.getAllTitle = this.getAllTitle.bind(this);
+        this.getAllAuthor = this.getAllAuthor.bind(this);
     }
 
     //componentDidMount() {
     //    this.getAll();
     //}
 
-    getAll() {
+    getAllTitle() {
         let title = this.titleToSearch.value;
         const URL = "https://www.googleapis.com/books/v1/volumes?q=untitle:"+title+"&maxResults=10"
         // This code gets data from the remote server.
         fetch(URL)
             .then(response => response.json())
-
             // Data Retrieved.
             .then(data => {
-                console.log(JSON.stringify(data));
+                //console.log(JSON.stringify(data));
+                console.log(URL)
                 this.setState({ searchByTitle: data.items });
+                console.log(URL)
+            })
+
+            // Data Not Retrieved.
+            .catch(error => {
+                alert(error);
+            });
+    }
+
+    getAllAuthor() {
+        let author = this.auhtorToSearch.value;
+        const URL = "https://www.googleapis.com/books/v1/volumes?q=unauthor:" + author + "&maxResults=10"
+        // This code gets data from the remote server.
+        fetch(URL)
+            .then(response => response.json())
+            // Data Retrieved.
+            .then(data => {
+                //console.log(JSON.stringify(data));
+                console.log(URL)
+                this.setState({ searchByAuthor: data.items });
+                console.log(URL)
             })
 
             // Data Not Retrieved.
@@ -37,9 +60,10 @@ export class Search extends Component {
             <div>
                 <input
                     type="text"
+                    placeholder= "title"
                     ref={getTitleInput => (this.titleToSearch = getTitleInput)}
                 />
-                <button onClick={this.getAll}>Search</button>
+                <button onClick={this.getAllTitle}>Search By Title</button>
                 <br />
               <table class="table table-sm table-dark">
                 <tr><th>Title</th><th>Author</th><th>Rating</th><th>Image</th></tr>
@@ -50,10 +74,28 @@ export class Search extends Component {
                             <td>  {data.volumeInfo.authors[0]} </td>
                             <td>  {data.volumeInfo.averageRating} </td>
                             <td>  <img src={data.volumeInfo.imageLinks.thumbnail} alt="coverimage" height="100" width="140"/> </td>
-
                         </tr>
                     )}
               </table>
+                <input
+                    type="text"
+                    placeholder="author"
+                    ref={getAuthorInput => (this.auhtorToSearch = getAuthorInput)}
+                />
+                <button onClick={this.getAllAuthor}>Search by Author</button>
+                <br />
+                <table class="table table-sm table-dark">
+                    <tr><th>Title</th><th>Author</th><th>Rating</th><th>Image</th></tr>
+                    {
+                        this.state.searchByAuthor.map((data) =>
+                            <tr>
+                                <td>  {data.volumeInfo.title} </td>
+                                <td>  {data.volumeInfo.authors[0]} </td>
+                                <td>  {data.volumeInfo.averageRating} </td>
+                                <td>  <img src={data.volumeInfo.imageLinks.thumbnail} alt="coverimage" height="100" width="140" /> </td>
+                            </tr>
+                        )}
+                </table>
             </div>
             );
     }
