aaf833c108c2d5dd2fd36a5b1fb1b50d650b3fd0
Sara <sbanaeirad@my.bcit.ca>
2020-02-28 13:55:34 -0800

    List table is added

--- Bookafe/Bookafe/Data/ApplicationDbContext.cs
+++ Bookafe/Bookafe/Data/ApplicationDbContext.cs
@@ -5,17 +5,36 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Bookafe.Data
+
+
 {
+
+    public class List
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string bookTitle { get; set; }
+        public bool IsComplete { get; set; }
+        public string userEmail { get; set; }
+    }
+
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
         public ApplicationDbContext(
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
+
         }
+
+        public DbSet<List> Lists { get; set; }
+
+
     }
 }
